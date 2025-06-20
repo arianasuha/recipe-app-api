5523727fef89300300d317c8080c1a94dba4fea1
Ariana Suha <arianahoque94@gmail.com>
2025-06-20 21:30:34 +0600

    Added Github Actions.

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator Functions
+"""
+
+def add(x, y):
+    """Ass x and y and resturn result"""
+    return x + y
+
+
+def subtract(x, y):
+    """subtract x from y and return result."""
+    return 0
