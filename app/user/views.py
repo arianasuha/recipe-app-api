ae2631b15acd8943d17c3bf1a8aa4595ac8fd798
Ariana Suha <arianahoque94@gmail.com>
2025-06-30 23:15:36 +0600

    Added new models

--- app/user/views.py
+++ app/user/views.py
@@ -18,8 +18,8 @@ class CreateUserView(generics.CreateAPIView):
 
 class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user."""
-    serializer_class = AuthTokenSerializer   #for input handling
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES  #for output formatting
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
